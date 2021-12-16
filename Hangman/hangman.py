ced447ecbf57820cdf37334314b6d594ec8cc23d
Anna <g.v.skudritskas@student.khai.edu>
2021-12-16 10:01:20 +0200

    Hangman 2-nd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -3,3 +3,18 @@ def many():
     print("HANGMAN")
     print("The game will be soon")
 
+
+# 2 этап
+def guess():
+    input("Press Enter to start the game: ")
+    print("Guess the word: ")
+    ans = input()
+
+    if ans == "python":
+        print("You survived!")
+    else:
+        print("You lost!")
+
+
+many()
+guess()
