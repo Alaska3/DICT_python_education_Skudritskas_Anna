6a952b4515e801b00992dd61aab551274105a66a
Anna <g.v.skudritskas@student.khai.edu>
2021-12-16 10:32:14 +0200

    Hangman 3-rd stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,16 +1,23 @@
+import random
+
+
 # 1 этап
 def many():
     print("HANGMAN")
     print("The game will be soon")
 
 
+word_list = ['python', 'java', 'javascript', 'php']  # 3 этап
+
+
 # 2 этап
 def guess():
     input("Press Enter to start the game: ")
     print("Guess the word: ")
+    ans_robot = random.choice(word_list)
     ans = input()
 
-    if ans == "python":
+    if ans == ans_robot:
         print("You survived!")
     else:
         print("You lost!")
