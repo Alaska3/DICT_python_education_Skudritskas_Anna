bb0d5079dc52668c3bb32e1e6c53794bd64ebe14
Anna <g.v.skudritskas@student.khai.edu>
2021-12-17 10:47:56 +0200

    Hangman 8-th stage

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -2,7 +2,7 @@ import random
 
 
 # 1 этап
-def many():  #
+def menu():  #
     print("HANGMAN")
     print("The game will be soon")
 
@@ -19,23 +19,24 @@ def guess():
     word = random.choice(word_list)
     splitted_word = list(word)
     guessed = ["-" for i in splitted_word]
-    english_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'l', 'g', 'k', 'j', 'i', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    while constant > 0:
+    english_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'l', 'g', 'k', 'j',
+                       'i', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    while True:
         print(' '.join(guessed))
         print("Attempts: ", constant)
         answer = input("Enter a latter: ").strip()
+        if len(answer) >= 2:
+            print("You should input a single letter")
+            constant = constant
+            continue
         if answer not in english_letters:
             print("Please, enter a lowercase English letter")
             constant = constant
             continue
-        elif len(answer) >= 2:
-            print("You should input a single letter")
-            constant = constant
-            continue
+
         if answer in guessed:
             constant -= 1
             print("You've already guessed this letter")
-
         elif answer in splitted_word:
             for i, c in enumerate(splitted_word):
                 if c == answer:
@@ -45,16 +46,21 @@ def guess():
                 print("You guessed the word!")
                 print("You survived!")
                 break
+
+            if constant == 0:
+                print("You lost, try again")
         else:
             print("That letter doesn't appear word")
             constant -= 1
 
-        if constant == 0:
-            print("You lost")
 
-
-
-
-
-many()
+menu()
 guess()
+# 8 этап
+while True:
+    user = input("Type 'play' to play the game, 'exit' to quit: ")
+
+    if user == 'play':
+        guess()
+    else:
+        break
