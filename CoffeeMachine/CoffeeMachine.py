fd5288a0bbf3e276e555282389b4385d97b6110c
Anna <g.v.skudritskas@student.khai.edu>
2021-12-19 15:29:08 +0200

    CoffeeMachine 2-nd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -6,3 +6,12 @@ print("Mixing boiled water with crushed coffee beans.")
 print("Pouring coffee into the cup.")
 print("Pouring some milk into the cup.")
 print("Coffee ready!")
+# 2 stage
+user = int(input("How many cups of coffee you need: "))
+print("For", user, "cups of coffee you will need: ")
+water = 200 * user
+milk = 50 * user
+coffee_beans = 15 * user
+print(water, "ml of water")
+print(milk, "ml of milk")
+print(coffee_beans, "g of coffee beans")
