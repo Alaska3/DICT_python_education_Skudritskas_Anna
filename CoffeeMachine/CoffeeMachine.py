e28a0f745f3bf958895e726aec8cad3a20703d0d
Anna <g.v.skudritskas@student.khai.edu>
2021-12-19 17:19:41 +0200

    CoffeeMachine 3-rd stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -6,12 +6,26 @@ print("Mixing boiled water with crushed coffee beans.")
 print("Pouring coffee into the cup.")
 print("Pouring some milk into the cup.")
 print("Coffee ready!")
-# 2 stage
-user = int(input("How many cups of coffee you need: "))
-print("For", user, "cups of coffee you will need: ")
-water = 200 * user
-milk = 50 * user
-coffee_beans = 15 * user
-print(water, "ml of water")
-print(milk, "ml of milk")
-print(coffee_beans, "g of coffee beans")
+# 3 stage
+water = 200
+milk = 50
+coffee_beans = 15
+print("Write how many ml of water the coffee machine has: ")
+a = int(input())
+print("Write, how many ml of milk the coffee machine has:")
+b = int(input())
+print("Write, how many grams of coffee beans the coffee machine has: ")
+c = int(input())
+print("Write, how many cups of coffee you will need: ")
+coffee_cups = int(input())
+water1 = a // water
+milk1 = b // milk
+coffee_beans1 = c // coffee_beans
+number_cups = min([water1, milk1, coffee_beans1])
+d = number_cups - coffee_cups
+if coffee_cups == number_cups:
+    print("Yes, I can make that amount of coffee.")
+elif coffee_cups < number_cups:
+    print("Yes, I can make that amount of coffee(and even", d, "more than that).")
+else:
+    print("No, I can make only", number_cups, "cups of coffee")
