c707033cb3ca01cd4161d69da53dd56acd49ceba
Anna <g.v.skudritskas@student.khai.edu>
2021-12-21 00:35:12 +0200

    CoffeeMachine 5-th stage

--- CoffeeMachine/CoffeeMachine.py
+++ CoffeeMachine/CoffeeMachine.py
@@ -6,91 +6,113 @@ print("Pouring coffee into the cup.")
 print("Pouring some milk into the cup.")
 print("Coffee ready!")
 
-# 4 stage
+# 5 stage
 money = 550
 water = 400
 milk = 540
 coffee_beans = 120
-caps = 9
+cups = 9
 
 
-def include():
+def remaining():
     print("The coffee machine has: ")
     print(str(water), "of water")
     print(str(coffee_beans), "of coffee beans")
-    print(str(caps), "of disposable caps")
+    print(str(cups), "of disposable caps")
     print(str(money), "of money")
 
 
-print()
+def number():
+    if water < 0:
+        print("Not enough water.")
+        return False
+    elif milk < 0:
+        print("Not enough milk.")
+        return False
+    elif coffee_beans < 0:
+        print("Not enough coffee beans.")
+        return False
+    elif cups < 0:
+        print("Not enough caps.")
+        return False
+    elif money < 0:
+        print("Not enough money.")
+        return False
+    else:
+        return True
 
 
 def buy():
-    global water
-    global coffee_beans
-    global milk
-    global money
-    print("Please, select the type of coffee: 1 - espresso, 2 - latte, 3 - cappuccino")
-    user = int(input())
-    if user == 1:
-        water -= 250
-        coffee_beans -= 16
-        money -= 4
-    elif user == 2:
-        water -= 350
-        milk -= 75
-        coffee_beans -= 20
-        money += 7
-    elif user == 3:
-        water -= 350
-        milk -= 75
-        coffee_beans -= 20
-        money += 6
-
-
-print()
+    global water, coffee_beans, milk, cups, money
+    print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:")
+    user_input = input()
+    if int(user_input) == 1:
+        if number():
+            water -= 250
+            coffee_beans -= 16
+            cups -= 1
+            money += 4
+            print("I have enough resources making you a espresso.")
+    elif int(user_input) == 2:
+        if number():
+            water -= 350
+            milk -= 75
+            coffee_beans -= 20
+            cups -= 1
+            money += 7
+            print("I have enough resources making you a latte.")
+    elif int(user_input) == 3:
+        if number():
+            water -= 350
+            milk -= 75
+            coffee_beans -= 20
+            cups -= 1
+            money += 6
+            print("I have enough resources making you a cappuccino.")
 
 
 def fill():
-    global water
-    global milk
-    global coffee_beans
+    global water, milk, coffee_beans, cups
     print("How many ml of water to add to the coffee machine?")
     water_add = int(input())
     water += water_add
     print("How many ml of milk to add to the coffee machine?")
     milk_add = int(input())
     milk += milk_add
+    print("How many cup to add to the coffee machine?")
+    cups_add = int(input())
+    cups = cups_add
     print("How many grams of coffee beans to add to the coffee machine?")
     beans_add = int(input())
     coffee_beans += beans_add
 
 
-print()
-
-
 def take():
     global money
-    print("I gave you", str(money), "/n")
+    print("I gave you", str(money), "\n")
     money = 0
 
 
-print()
-
-
 def option():
-    include()
-    print("Please, choose options: 'buy', 'fill', 'take' ")
-    user = input()
-    if user == 'buy':
-        buy()
-        include()
-    elif user == 'fill':
-        fill()
-        include()
-    elif user == 'take':
-        take()
-        include()
+    while True:
+        remaining()
+        print("Please, choose options: 'buy', 'fill', 'take', 'remaining'")
+        user_input = input()
+        if user_input == 'buy':
+            buy()
+        elif user_input == 'fill':
+            fill()
+        elif user_input == 'take':
+            take()
+        elif user_input == 'remaining':
+            remaining()
+
+        print("What ou want to do next: 'back', 'exit'")
+        user_next_input = input()
+        if user_next_input == 'back':
+            continue
+        elif user_next_input == 'exit':
+            break
 
 
 option()
