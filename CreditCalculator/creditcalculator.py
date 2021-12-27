acac44174c9f0612c5f220d1321f076cfae07428
Anna <g.v.skudritskas@student.khai.edu>
2021-12-27 12:21:36 +0200

    CreditCalculator 3-rd stage

--- CreditCalculator/creditcalculator.py
+++ CreditCalculator/creditcalculator.py
@@ -1,4 +1,5 @@
 import math
+
 print("Loan principal: 1000")
 print("Month 1: repaid 250")
 print("Month 2: repaid 250")
@@ -8,20 +9,44 @@ print("The loan has been repaid!")
 print("Enter the loan principal: ")
 q = int(input())
 print("What do you want to calculate? ")
-print("type 'm' - for number of monthly payments,"
-      "type 'p' - for the monthly payment:")
+print("type 'n' - for number of monthly payments,"
+      "type 'a' - for annuity monthly payment amount,"
+      "type 'p' - for loan principal:")
 user1 = input()
-if user1 == 'm':
-    print("Enter the monthly payment: ")
-    q_m = int(input())
-    payment = math.ceil(q/q_m)
-    print("It will take " + str(payment) + " months to repay the loan")
-else:
+if user1 == 'n':
+    print("Enter the loan principal: ")
+    p = int(input())
     print("Enter the monthly payment: ")
     q_n = int(input())
-    payment1 = math.ceil(q / q_n)
-    last_payment = q - (q_n - 1) * payment1
-    if last_payment == payment1:
-        print("Your monthly payment = " + str(payment1))
-    else:
-        print("Your monthly payment = " + str(payment1) + "and the last payment = " + str(last_payment))
+    print("Enter the loan interest: ")
+    l = int(input()) / 1200
+    payment = math.ceil(math.log(q_n / (q_n - (l * p), (1 + l))))
+    if payment % 12 == 0:
+        s = payment // 12
+        print("It will take " + str(s) + "months to repay this loan!")
+    elif payment % 12 != 0:
+        s = payment // 12
+        k = payment % 12
+        print("It will take " + str(s) + "years and " + str(k) + "months to repay this loan!")
+    elif payment // 12 == 0:
+        print("It will take " + str(payment) + "years to repay this loan!")
+elif user1 == 'a':
+    print("Enter the loan principal: ")
+    p = float(input())
+    print("Enter the number of periods: ")
+    n = float(input())
+    print("Enter the loan interest: ")
+    l = float(input()) / 1200
+    i_n = (1 + l) ** n
+    a = math.ceil(p * (l * i_n / (i_n - 1)))
+    print("Your monthly payment = " + str(a) + "!")
+elif user1 == 'p':
+    print("Enter the annuity payment: ")
+    a = float(input())
+    print("Enter the number of periods: ")
+    k = float(input())
+    print("Enter the loan interest: ")
+    l = float(input()) / 1200
+    l_k = (1 + l) ** k
+    p = round(a / (l * l_k / (l_k - 1)))
+    print("Your loan principal = " + str(p) + "!")
