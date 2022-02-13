35b8bfe206b202bdd6f99144bd6dc3b53729d9b7
Ann <g.v.skudritskas@student.khai.edu>
2022-02-13 21:55:51 +0200

    DinnerParty 3-rd stage

--- DinnerParty/DinnerParty.py
+++ DinnerParty/DinnerParty.py
@@ -1,3 +1,4 @@
+import random
 print("Enter the number of friends joining (including you): ")
 number = int(input())
 a = {}
@@ -12,7 +13,24 @@ else:
 
 print("Enter the total amount: ")
 bill = int(input())
-bill1 = bill / number
+bill1 = round(bill / number, 2)
 for name in a:
     a[name] = bill1
+    
 print(a)
+
+print("Do you want to use the 'Who is lucky?' feature? Write Yes/No: ")
+user = input()
+if user == 'Yes':
+    my_list = list(a.keys())
+    r = random.choice(my_list)
+    my_list1 = [r]
+    new_d = dict.fromkeys(my_list1, 0)
+    print(new_d)
+    print(r, "is the lucky one!")
+    a.update(new_d)
+    print(a)
+elif user == 'No':
+    print("No one is going to be lucky.")
+else:
+    print("Please, write Yes/No.")
