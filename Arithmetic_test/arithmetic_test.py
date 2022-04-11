9e13a3c291ce7a889c6df39e67e280dc3a972f81
Anna <g.v.skudritskas@student.khai.edu>
2022-04-11 19:28:14 +0300

    Arithmetic_test 2-nd stage

--- Arithmetic_test/arithmetic_test.py
+++ Arithmetic_test/arithmetic_test.py
@@ -1,13 +1,27 @@
 from random import *
-num1 = randint(2, 9)
-num2 = randint(2, 9)
-my_list = [str(num1) + '*' + str(num2), str(num1) + '-' + str(num2), str(num1) + '+' + str(num2)]
-rand = choice(my_list)
-t = eval(rand)   # результат выполнения строки
-print(rand)
-user = int(input())
-
-if user == t:
-    print("Right!")
-else:
-    print("Wrong!")
+i = 0
+n = 0
+while i < 5:
+    i += 1
+    num1 = randint(2, 9)
+    num2 = randint(2, 9)
+    my_list = [str(num1) + '*' + str(num2), str(num1) + '-' + str(num2), str(num1) + '+' + str(num2)]
+    rand = choice(my_list)
+    t = eval(rand)  # результат выполнения строки
+    print(rand)
+    user = input()
+    if not user:
+        print("Incorrect format!")
+        i -= 1
+        continue
+    try:
+        ans = int(user)
+        if ans == t:
+            n += 1
+            print('Right')
+        else:
+            print('Wrong')
+    except ValueError:
+        print("Incorrect format!")
+        i -= 1
+print("Your mark", n, "/ 5")
